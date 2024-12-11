Fix scaling law visualization plot

notebooks/00_bon_jailbreaking_eda/00_bon_jailbreaking_eda.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from typing import Any, Dict, List, Tuple
 
 import src.analyze
+import src.globals
 import src.plot
 import src.utils
 
@@ -17,8 +18,8 @@ data_dir, results_dir = src.utils.setup_notebook_dir(
 )
 
 bon_jailbreaking_pass_at_k_df = src.analyze.create_or_load_bon_jailbreaking_pass_at_k_df(
-    # refresh=False,
-    refresh=True,
+    refresh=False,
+    # refresh=True,
 )
 
 plt.close()
@@ -43,10 +44,21 @@ src.plot.save_plot_with_multiple_extensions(
 )
 # plt.show()
 
+bon_jailbreaking_neg_log_avg_pass_at_k_df = (
+    bon_jailbreaking_pass_at_k_df.groupby(["Model", "Modality", "Scaling Parameter"])[
+        "Score"
+    ]
+    .mean()
+    .reset_index()
+)
+bon_jailbreaking_neg_log_avg_pass_at_k_df["Neg Log Score"] = -np.log(
+    bon_jailbreaking_neg_log_avg_pass_at_k_df["Score"]
+)
+
 plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
-    data=bon_jailbreaking_pass_at_k_df,
+    data=bon_jailbreaking_neg_log_avg_pass_at_k_df,
     x="Scaling Parameter",
     y="Neg Log Score",
     hue="Model",
@@ -57,8 +69,8 @@ g.set(
     xscale="log",
     yscale="log",
     ylim=(1e-3, None),
-    xlabel="Scaling Parameter" + r"(Num. Attempts $N$)",
-    ylabel=r"$-\log (\text{Attack Success Rate})$",
+    xlabel=r"Scaling Parameter (Num. Attempts $N$)",
+    ylabel=r"$-\log (\mathbb{E}[\text{Attack Success Rate}])$",
 )
 sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1.04))
 src.plot.save_plot_with_multiple_extensions(
@@ -66,6 +78,7 @@ src.plot.save_plot_with_multiple_extensions(
     plot_filename="y=neg_log_score_vs_x=scaling_parameter_hue=model",
 )
 # plt.show()
+
 
 plt.close()
 g = sns.relplot(
@@ -75,14 +88,16 @@ g = sns.relplot(
     y="Score",
     units="Problem Idx",
     hue="Model",
+    hue_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     col="Model",
+    col_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     col_wrap=4,
     estimator=None,
 )
 g.set(
     xscale="log",
     ylabel="Attack Success Rate",
-    xlabel="Scaling Parameter" + r"(Num. Attempts $N$)",
+    xlabel=r"Scaling Parameter (Num. Attempts $N$)",
 )
 # Move legend to the empty subplot position
 g._legend.set_bbox_to_anchor((0.95, 0.25))
@@ -103,7 +118,9 @@ g = sns.relplot(
     y="Neg Log Score",
     units="Problem Idx",
     hue="Model",
+    hue_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     col="Model",
+    col_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     col_wrap=4,
     estimator=None,
 )
@@ -147,8 +164,10 @@ g = sns.displot(
     kind="hist",
     x="Score",
     hue="Model",
+    hue_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     bins=all_bins,
     col="Model",
+    col_order=src.globals.BON_JAILBREAKING_MODELS_ORDER,
     col_wrap=4,
 )
 g.set(
@@ -165,5 +184,6 @@ src.plot.save_plot_with_multiple_extensions(
     plot_filename="y=counts_x=score_hue=model_col=model_bins=custom",
 )
 # plt.show()
+
 
 print("Finished notebooks/00_bon_jailbreaking_eda/00_bon_jailbreaking_eda.py!")

notebooks/01_large_language_monkeys_eda/01_large_language_monkeys_eda.py
@@ -46,10 +46,23 @@ src.plot.save_plot_with_multiple_extensions(
 )
 # plt.show()
 
+
+large_language_monkeys_neg_log_avg_pass_at_k_df = (
+    large_language_monkeys_pass_at_k_df.groupby(
+        ["Model", "Benchmark", "Scaling Parameter"]
+    )["Score"]
+    .mean()
+    .reset_index()
+)
+large_language_monkeys_neg_log_avg_pass_at_k_df["Neg Log Score"] = -np.log(
+    large_language_monkeys_neg_log_avg_pass_at_k_df["Score"]
+)
+
+
 plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
-    data=large_language_monkeys_pass_at_k_df,
+    data=large_language_monkeys_neg_log_avg_pass_at_k_df,
     x="Scaling Parameter",
     y="Neg Log Score",
     hue="Model",
@@ -60,7 +73,7 @@ g.set(
     title="Large Language Monkeys",
     xscale="log",
     yscale="log",
-    ylim=(1e-3, None),
+    ylim=(1e-1, None),
     xlabel=r"Scaling Parameter (Num. Attempts $k$)",
     ylabel=r"$-\log (\mathbb{E}[\text{Coverage}])$",
 )
@@ -80,6 +93,7 @@ g = sns.relplot(
     y="Score",
     units="Problem Idx",
     hue="Model",
+    hue_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     col="Model",
     col_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     col_wrap=4,
@@ -109,6 +123,7 @@ g = sns.relplot(
     y="Neg Log Score",
     units="Problem Idx",
     hue="Model",
+    hue_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     col="Model",
     col_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     col_wrap=4,
@@ -157,6 +172,7 @@ g = sns.displot(
     hue_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     bins=all_bins,
     col="Model",
+    col_order=src.globals.LARGE_LANGUAGE_MONKEYS_PYTHIA_MODELS_ORDER,
     col_wrap=4,
 )
 g.set(

notebooks/02_large_language_monkeys_original_eda/02_large_language_monkeys_original_eda.py
@@ -17,16 +17,16 @@ data_dir, results_dir = src.utils.setup_notebook_dir(
     refresh=False,
 )
 
-large_language_monkeys_pass_at_k_df = src.analyze.create_or_load_large_language_monkeys_original_pass_at_k_df(
-    # refresh=False,
-    refresh=True,
+large_language_monkeys_original_pass_at_k_df = src.analyze.create_or_load_large_language_monkeys_original_pass_at_k_df(
+    refresh=False,
+    # refresh=True,
 )
 
 
 plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
-    data=large_language_monkeys_pass_at_k_df,
+    data=large_language_monkeys_original_pass_at_k_df,
     x="Scaling Parameter",
     y="Score",
     hue="Model",
@@ -47,10 +47,22 @@ src.plot.save_plot_with_multiple_extensions(
 )
 # plt.show()
 
+large_language_monkeys_original_neg_log_avg_pass_at_k_df = (
+    large_language_monkeys_original_pass_at_k_df.groupby(
+        ["Model", "Benchmark", "Scaling Parameter"]
+    )["Score"]
+    .mean()
+    .reset_index()
+)
+large_language_monkeys_original_neg_log_avg_pass_at_k_df["Neg Log Score"] = -np.log(
+    large_language_monkeys_original_neg_log_avg_pass_at_k_df["Score"]
+)
+
+
 plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
-    data=large_language_monkeys_pass_at_k_df,
+    data=large_language_monkeys_original_neg_log_avg_pass_at_k_df,
     x="Scaling Parameter",
     y="Neg Log Score",
     hue="Model",
@@ -61,7 +73,7 @@ g.set(
     title="Large Language Monkeys (Original)",
     xscale="log",
     yscale="log",
-    ylim=(1e-3, None),
+    ylim=(1e-1, None),
     xlabel=r"Scaling Parameter (Num. Attempts $k$)",
     ylabel=r"$-\log (\mathbb{E}[\text{Coverage}])$",
 )
@@ -75,7 +87,7 @@ src.plot.save_plot_with_multiple_extensions(
 
 plt.close()
 g = sns.relplot(
-    data=large_language_monkeys_pass_at_k_df,
+    data=large_language_monkeys_original_pass_at_k_df,
     kind="line",
     x="Scaling Parameter",
     y="Score",
@@ -104,7 +116,7 @@ src.plot.save_plot_with_multiple_extensions(
 
 plt.close()
 g = sns.relplot(
-    data=large_language_monkeys_pass_at_k_df,
+    data=large_language_monkeys_original_pass_at_k_df,
     kind="line",
     x="Scaling Parameter",
     y="Neg Log Score",
@@ -134,8 +146,8 @@ src.plot.save_plot_with_multiple_extensions(
 
 plt.close()
 # Create better bins that handle zero and near-zero values
-smallest_nonzero_pass_at_1 = large_language_monkeys_pass_at_k_df[
-    large_language_monkeys_pass_at_k_df["Score"] > 0.0
+smallest_nonzero_pass_at_1 = large_language_monkeys_original_pass_at_k_df[
+    large_language_monkeys_original_pass_at_k_df["Score"] > 0.0
 ]["Score"].min()
 # Round smallest_nonzero_value to the nearest power of 10.
 smallest_nonzero_pass_at_1 = 10.0 ** np.floor(np.log10(smallest_nonzero_pass_at_1))
@@ -148,9 +160,9 @@ all_bins = np.concatenate(
     [[-small_value_for_plotting], [small_value_for_plotting], log_bins]
 )
 g = sns.displot(
-    data=large_language_monkeys_pass_at_k_df[
-        (large_language_monkeys_pass_at_k_df["Scaling Parameter"] == 1)
-        * (large_language_monkeys_pass_at_k_df["Benchmark"] == "MATH")
+    data=large_language_monkeys_original_pass_at_k_df[
+        (large_language_monkeys_original_pass_at_k_df["Scaling Parameter"] == 1)
+        * (large_language_monkeys_original_pass_at_k_df["Benchmark"] == "MATH")
     ],
     kind="hist",
     x="Score",