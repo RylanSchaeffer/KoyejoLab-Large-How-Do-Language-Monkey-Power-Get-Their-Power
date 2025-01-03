9795b089631ea43258f41940cbe430dc754284fb
Rylan <rylanschaeffer@gmail.com>
2025-01-03 14:02:55 -0800

    Added Monkeys MiniF2F-MATH

--- src/analyze.py
+++ src/analyze.py
@@ -1433,6 +1433,150 @@ def create_or_load_large_language_monkeys_code_contests_pass_at_k_df(
     return large_language_monkeys_code_contests_pass_at_k_df
 
 
+def create_or_load_large_language_monkeys_mini_f2f_individual_outcomes_df(
+    raw_data_dir=f"{os.getcwd()}/data/raw_data",
+    processed_data_dir=f"{os.getcwd()}/data/processed_data",
+    refresh: bool = False,
+) -> pd.DataFrame:
+    large_language_monkeys_mini_f2f_individual_outcomes_df_path = os.path.join(
+        processed_data_dir,
+        "large_language_monkeys_mini_f2f_individual_outcomes.parquet",
+    )
+
+    if refresh or not os.path.exists(
+        large_language_monkeys_mini_f2f_individual_outcomes_df_path
+    ):
+        print(
+            f"Creating {large_language_monkeys_mini_f2f_individual_outcomes_df_path} anew..."
+        )
+
+        os.makedirs(processed_data_dir, exist_ok=True)
+        large_language_monkeys_original_dfs_list = []
+        subsets = [
+            "MiniF2F-MATH_Llama-3-8B-Instruct",
+            "MiniF2F-MATH_Llama-3-70B-Instruct",
+        ]
+        for subset in subsets:
+            benchmark, model = subset.split("_")
+            ds = load_dataset("ScalingIntelligence/monkey_business", subset)["test"]
+            correct: List[List[bool]] = ds["is_corrects"]
+            # Shape: (128, 10000)
+            wide_df = pd.DataFrame(
+                correct,
+                columns=1 + np.arange(10000),
+                dtype=np.float16,
+            )
+            # Convert to floats.
+            wide_df = wide_df.astype(np.float16)
+            wide_df["Problem Idx"] = ds["orig_dset_idx"]
+            df = wide_df.melt(
+                id_vars=["Problem Idx"],
+                var_name="Attempt Idx",
+                value_name="Score",
+            )
+
+            df["Benchmark"] = benchmark
+            # Convert, e.g., "Pythia-1.4B" to "Pythia 1.4B".
+            df["Model"] = model.replace("-", " ")
+            large_language_monkeys_original_dfs_list.append(df)
+
+        large_language_monkeys_original_individual_outcomes_df = pd.concat(
+            large_language_monkeys_original_dfs_list,
+        )
+        large_language_monkeys_original_individual_outcomes_df[
+            "Attempt Idx"
+        ] = pd.to_numeric(
+            large_language_monkeys_original_individual_outcomes_df["Attempt Idx"]
+        )
+
+        large_language_monkeys_original_individual_outcomes_df.to_parquet(
+            large_language_monkeys_mini_f2f_individual_outcomes_df_path,
+            index=False,
+        )
+
+        print(
+            f"Wrote {large_language_monkeys_mini_f2f_individual_outcomes_df_path} to disk."
+        )
+        del large_language_monkeys_original_individual_outcomes_df
+
+    large_language_monkeys_original_individual_outcomes_df = pd.read_parquet(
+        large_language_monkeys_mini_f2f_individual_outcomes_df_path
+    )
+    print(
+        f"Loaded {large_language_monkeys_mini_f2f_individual_outcomes_df_path} with shape: ",
+        large_language_monkeys_original_individual_outcomes_df.shape,
+    )
+    return large_language_monkeys_original_individual_outcomes_df
+
+
+def create_or_load_large_language_monkeys_mini_f2f_pass_at_k_df(
+    raw_data_dir=f"{os.getcwd()}/data/raw_data",
+    processed_data_dir=f"{os.getcwd()}/data/processed_data",
+    refresh: bool = False,
+) -> pd.DataFrame:
+    large_language_monkeys_mini_f2f_pass_at_k_df_path = os.path.join(
+        processed_data_dir,
+        "large_language_monkeys_mini_f2f_pass_at_k.parquet",
+    )
+
+    if refresh or not os.path.exists(large_language_monkeys_mini_f2f_pass_at_k_df_path):
+        print(f"Creating {large_language_monkeys_mini_f2f_pass_at_k_df_path} anew...")
+
+        large_language_monkeys_mini_f2f_individual_outcomes_df = (
+            create_or_load_large_language_monkeys_mini_f2f_individual_outcomes_df(
+                raw_data_dir=raw_data_dir,
+                processed_data_dir=processed_data_dir,
+                refresh=refresh,
+            )
+        )
+
+        large_language_monkeys_mini_f2f_num_samples_and_num_successes_df = convert_individual_outcomes_to_num_samples_and_num_successes_df(
+            individual_outcomes_df=large_language_monkeys_mini_f2f_individual_outcomes_df,
+            groupby_cols=["Model", "Benchmark", "Problem Idx"],
+        )
+
+        pass_at_k_dfs_list = []
+        for (
+            model,
+            benchmark,
+        ), subset_num_samples_and_num_successes_df in large_language_monkeys_mini_f2f_num_samples_and_num_successes_df.groupby(
+            ["Model", "Benchmark"]
+        ):
+            pass_at_k_df = compute_pass_at_k_from_num_samples_and_num_successes_df(
+                num_samples_and_num_successes_df=subset_num_samples_and_num_successes_df,
+                ks_list=src.globals.LARGE_LANGUAGE_MONKEYS_ORIGINAL_Ks_LIST,
+            )
+            pass_at_k_df["Model"] = model
+            pass_at_k_df["Benchmark"] = benchmark
+            pass_at_k_dfs_list.append(pass_at_k_df)
+
+        large_language_monkeys_mini_f2f_pass_at_k_df = pd.concat(
+            pass_at_k_dfs_list, ignore_index=True
+        )
+        large_language_monkeys_mini_f2f_pass_at_k_df["Log Score"] = np.log(
+            large_language_monkeys_mini_f2f_pass_at_k_df["Score"]
+        )
+        large_language_monkeys_mini_f2f_pass_at_k_df[
+            "Neg Log Score"
+        ] = -large_language_monkeys_mini_f2f_pass_at_k_df["Log Score"]
+        large_language_monkeys_mini_f2f_pass_at_k_df.to_parquet(
+            large_language_monkeys_mini_f2f_pass_at_k_df_path,
+            index=False,
+        )
+
+        print(f"Wrote {large_language_monkeys_mini_f2f_pass_at_k_df_path} to disk.")
+        del large_language_monkeys_mini_f2f_pass_at_k_df
+
+    large_language_monkeys_mini_f2f_pass_at_k_df = pd.read_parquet(
+        large_language_monkeys_mini_f2f_pass_at_k_df_path
+    )
+    print(
+        f"Loaded {large_language_monkeys_mini_f2f_pass_at_k_df_path} with shape: ",
+        large_language_monkeys_mini_f2f_pass_at_k_df.shape,
+    )
+    return large_language_monkeys_mini_f2f_pass_at_k_df
+
+
 def create_or_load_large_language_monkeys_pythia_math_beta_binomial_mle_df(
     raw_data_dir=f"{os.getcwd()}/data/raw_data",
     processed_data_dir=f"{os.getcwd()}/data/processed_data",
